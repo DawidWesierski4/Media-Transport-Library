37de5aaec837f014cb81e46dc554a7a8c2d9d1f8
Wesierski, Dawid <dawid.wesierski@intel.com>
2025-03-31 07:31:12 -0400

    Test_ i dont' really want to do it anymore

--- repro_docker.sh
+++ repro_docker.sh
@@ -14,8 +14,10 @@ LOG_LEVEL=0
 FORMAT=I422_10LE
 INPUT_FOLDER=/mnt/ramdisk_src
 INPUT=${INPUT_FOLDER}/GSTREAMER_${WIDTH}_${HEIGHT}_${FORMAT}_${VIDEO_FPS}_${VIDEO_FPS_DIV}.raw
-BLOCKSIZE=33177600
+BLOCKSIZE=3110400
 GSTREAMER_PLUGINS_PATH=/home/mtl/gstreamer/
+IP_MULTICAST=239.168.75.30
+IP_MULTICAST2=239.168.75.32
 
 VFIO_PORT_1=0000:b1:11.0
 VFIO_PORT_2=0000:b1:11.1
@@ -30,6 +32,7 @@ IP_PORT_3=192.168.12.183
 IP_PORT_4=192.168.12.184
 IP_PORT_5=192.168.12.185
 IP_PORT_6=192.168.12.186
+NUMA_NODE=1
 
 set -xe
 init_test() {
@@ -45,7 +48,7 @@ init_test() {
     if [[ ! -f ${INPUT} ]]; then
 
         echo "Creating input file ${INPUT}"
-        dd if=/dev/zero of=${INPUT} bs=${BLOCKSIZE} count=$((VIDEO_FPS * 15 / VIDEO_FPS_DIV))
+        dd if=/dev/zero of=${INPUT} bs=${BLOCKSIZE} count=$((VIDEO_FPS * 5 / VIDEO_FPS_DIV))
     fi
 
 
@@ -56,6 +59,29 @@ init_test() {
         MtlManager &
         sleep 3
     fi
+
+    SCRIPT_BASENAME=$(basename $0)
+    SCRIPT_DIR=$(dirname $0)
+    LOG_DIR=${SCRIPT_DIR}/logs_${SCRIPT_BASENAME}
+
+    if [ ! -d $LOG_DIR ]; then
+        mkdir $LOG_DIR
+    fi
+
+    REPO_NUMBER_FILE=${LOG_DIR}/${SCRIPT_BASENAME}_repo_number
+    if [ -f $REPO_NUMBER_FILE ]; then
+        REPO_NUMBER=$(($(cat $REPO_NUMBER_FILE) + 1))
+    else
+        REPO_NUMBER=0
+    fi
+    echo $REPO_NUMBER > $REPO_NUMBER_FILE
+
+    LOG_FILE=${LOG_DIR}/repo_${REPO_NUMBER}.log
+    cat $0 > $LOG_FILE
+    exec > >(tee -a $LOG_FILE) 2>&1
+
+    echo "Initialized $SCRIPT_BASENAME test iteration $REPO_NUMBER"
+    echo "Log directory: $LOG_FILE"
 }
 
 function_test() {
@@ -112,8 +138,8 @@ function_test() {
                     dev-ip=$2  "
 }
 
-function_test2() {
-    docker run --rm \
+function_test_numa_force() {
+    numactl --cpunodebind=${NUMA_NODE} --membind=${NUMA_NODE} docker run --rm \
         --ulimit memlock=-1:-1 \
         --device /dev/vfio:/dev/vfio \
         --volume /var/run/imtl:/var/run/imtl \
@@ -159,23 +185,20 @@ if [[ ${BASH_SOURCE} == ${0} ]]; then
         echo "This script must be run as root" 
         exit 1
     fi
-    function_test2 $VFIO_PORT_1 $IP_PORT_2 &
-    function_test2 $VFIO_PORT_3 $IP_PORT_4 &
-    function_test2 $VFIO_PORT_5 $IP_PORT_6 &
-    function_test2 $VFIO_PORT_2 $IP_PORT_1 &
-    function_test2 $VFIO_PORT_4 $IP_PORT_3 &
-    function_test2 $VFIO_PORT_6 $IP_PORT_5 &
-    # funciton_local $VFIO_PORT_3 $IP_PORT_4 &
-    # funciton_local $VFIO_PORT_5 $IP_PORT_6 &
+    function_test $VFIO_PORT_1 $IP_PORT_2 &
+    function_test $VFIO_PORT_3 $IP_PORT_4 &
+    function_test $VFIO_PORT_5 $IP_PORT_6 &
+    function_test $VFIO_PORT_2 $IP_PORT_1 &
+    function_test $VFIO_PORT_4 $IP_PORT_3 &
+    function_test $VFIO_PORT_6 $IP_PORT_5 &
 
-
-
-    # function_test $VFIO_PORT_1 $IP_PORT_2 &
-    # function_test $VFIO_PORT_3 $IP_PORT_4 &
-    # function_test $VFIO_PORT_5 $IP_PORT_6 &
-    # function_test $VFIO_PORT_2 $IP_PORT_1 &
-    # function_test $VFIO_PORT_4 $IP_PORT_3 &
-    # function_test $VFIO_PORT_6 $IP_PORT_5 &
+    #numactl --cpunodebind=0 --membind=0
+    # function_test_numa_force $VFIO_PORT_1 $IP_PORT_2 &
+    # function_test_numa_force $VFIO_PORT_3 $IP_PORT_4 &
+    # function_test_numa_force $VFIO_PORT_5 $IP_PORT_6 &
+    # function_test_numa_force $VFIO_PORT_2 $IP_PORT_1 &
+    # function_test_numa_force $VFIO_PORT_4 $IP_PORT_3 &
+    # function_test_numa_force $VFIO_PORT_6 $IP_PORT_5 &
 
     # function_test_red $VFIO_PORT_1 $IP_PORT_2 $VFIO_PORT_2 $IP_PORT_1 &
     # function_test_red $VFIO_PORT_3 $IP_PORT_4 $VFIO_PORT_4 $IP_PORT_3 &
