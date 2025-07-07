fc56ac0f0db2205d32b56cc04e1646ff8d976486
Wesierski, Dawid <dawid.wesierski@intel.com>
2025-07-07 08:44:59 -0400

    init

--- lib/src/mt_usdt_provider.d
+++ lib/src/mt_usdt_provider.d
@@ -9,6 +9,7 @@ provider sys {
   probe log_msg(int level, char* msg);
   /* attach to enable the tasklet_time_measure at runtime */
   probe tasklet_time_measure();
+  probe tasklet_time_out_of_bounds_measure();
   /* attach to enable the sessions_time_measure at runtime */
   probe sessions_time_measure();
   /* attach to enable the pcap dump for cni rx queue */
---
+++ tracing.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo BPFTRACE_STRLEN=128 bpftrace -e '
+usdt::sys:sessions_time_measure {
+  printf("%s", strftime("%H:%M:%S", nsecs));
+}
+usdt::sys:sessions_time_measure
+{
+  printf("%s", strftime("%H:%M:%S", nsecs));
+}
+usdt::sys::tasklet_time_out_of_bounds_measure {
+  printf("%s", strftime("%H:%M:%S", nsecs));
+}
+' -p $(pgrep gst)
