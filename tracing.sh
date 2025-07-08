830a3024431b988e7a0a80f1a85897518a2a669d
Wesierski, Dawid <dawid.wesierski@intel.com>
2025-07-08 09:28:21 -0400

    devdev

--- tracing.sh
+++ tracing.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  process_name="gstreamer"
+else
+  process_name="$1"
+fi
+
+if ! $(pgrep ${process_name}) ; then
+  echo "NO ${process_name} WORKING PROCESS FOUND"
+  exit 1
+fi
+
 sudo BPFTRACE_MAX_STRLEN=235 bpftrace -e '
 usdt::sys:sessions_time_measure {
   printf("%s", strftime("%H:%M:%S", nsecs));
@@ -11,4 +22,4 @@ usdt::sys:sessions_time_measure
 usdt::sys:tasklet_time_out_of_bounds_measure {
     printf("%s", strftime("%H:%M:%S", nsecs));
 }
-' -p $(pgrep gst)
+' -p $(pgrep ${process_name})
